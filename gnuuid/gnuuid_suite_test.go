9d06213a6845007842a36f89dd77def03bf2b10a
Dmitry Mozzherin <dmozzherin@gmail.com>
2020-10-25 05:34:00 -0500

    add libs

---
+++ gnuuid/gnuuid_suite_test.go
@@ -0,0 +1,13 @@
+package gnuuid_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestUuid(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Uuid Suite")
+}

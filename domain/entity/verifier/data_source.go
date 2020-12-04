23b98aa4650a6755c507ec12503a5d8527ac0c5b
Dmitry Mozzherin <dmozzherin@gmail.com>
2020-12-04 16:26:28 -0600

    add isOutlinkReady field to DataSource

--- domain/entity/verifier/data_source.go
+++ domain/entity/verifier/data_source.go
@@ -1,7 +1,5 @@
 package verifier
 
-import "time"
-
 // DataSource provides metadata for an externally collected data-set.
 type DataSource struct {
 	// ID is a DataSource Id.
@@ -20,7 +18,7 @@ type DataSource struct {
 	Version string `json:"version,omitempty"`
 
 	// RevisionDate of a data-set from a data-provider.
-	// It follows format of 'year-month-day' || 'year-month' || 'year'
+	// It follows format of 'YYYY-MM-DD' || 'YYYY-MM' || 'YYYY'
 	// This data comes from the information given by the data-provider,
 	// while UpdatedAt field is the date of harvesting of the
 	// resource.
@@ -41,6 +39,13 @@ type DataSource struct {
 	// WebsiteURL is a hompage of a DataSource
 	WebsiteURL string `json:"homeURL,omitempty"`
 
+	// IsOutlinkReady is true for data-sources that have enough data and
+	// metadata to be recommended for outlinking by third-party applications
+	// (be included into preferred data-sources). When false, it does not
+	// mean that the original resource is not valuable, it means that
+	// its representation at gnames is not complete/resent enough.
+	IsOutlinkReady bool `json:"isOutlinkReady"`
+
 	// Curation determines how much of manual or programmatic work is put
 	// into assuring the quality of the data.
 	Curation CurationLevel `json:"curation"`
@@ -48,8 +53,8 @@ type DataSource struct {
 	// RecordCount tells how many entries are in a DataSource.
 	RecordCount int `json:"recordCount"`
 
-	// UpdatedAt is the last import time and date. In contrast,
+	// UpdatedAt is the last import date (YYYY-MM-DD). In contrast,
 	// RevisionDate field indicates when the resource was
 	// updated according to its data-provider.
-	UpdatedAt time.Time `json:"updatedAt"`
+	UpdatedAt string `json:"updatedAt"`
 }

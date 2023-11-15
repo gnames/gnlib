ffc8910ec9c7be93fbf6b05c303f1586d9ebc875
Dmitry Mozzherin <dmozzherin@gmail.com>
2023-11-15 09:06:22 +0000

    add generic Map function

---
+++ util.go
@@ -0,0 +1,10 @@
+package gnlib
+
+// Map applies a function to each element of a slice and returns a new slice
+func Map[T any, U any](s []T, f func(T) U) []U {
+	result := make([]U, len(s))
+	for i, v := range s {
+		result[i] = f(v)
+	}
+	return result
+}

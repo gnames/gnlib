5834686073223b803bfd6f3353f04ae0ad4183c0
Dmitry Mozzherin <dmozzherin@gmail.com>
2023-11-16 15:11:24 +0000

    swag documentation

--- ent/gnvers/gnvers.go
+++ ent/gnvers/gnvers.go
@@ -1,6 +1,12 @@
 package gnvers
 
+// @Description Version provides information about the version
+// @Description of an application.
 type Version struct {
-	Version string `json:"version"`
-	Build   string `json:"build"`
+	// Version specifies the version of the app, usually in the v0.0.0 format.
+	Version string `json:"version" example:"v1.0.2"`
+
+	// Build contains the timestamp or other details
+	// indicating when the app was compiled.
+	Build string `json:"build" example:"2023-08-03_18:58:38UTC"`
 }

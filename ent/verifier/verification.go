361f5159043788fc10466a3fe396805017719e10
Dmitry Mozzherin <dmozzherin@gmail.com>
2025-09-16 21:19:41 +0000

    improve comments in verification.go

--- ent/verifier/verification.go
+++ ent/verifier/verification.go
@@ -1,212 +1,211 @@
 package verifier
 
-// Input is options/parameters for the Verify method.
+// Input contains options and parameters for the Verify method.
 type Input struct {
-	// NameStrings is a list of name-strings to verify.
+	// NameStrings contains the list of name-strings to verify.
 	NameStrings []string `json:"nameStrings"`
 
-	// DataSources field contains DataSources IDs to limit results to only these
-	// sources. The best result is calculated only out of this limited set of
-	// data. By default only the BestResult is shown. To see all results use
-	// WithAllMatches flag.
+	// DataSources contains data source IDs to limit results to only these
+	// sources. The best result is calculated only from this limited set of
+	// data. By default only the best result is shown. To see all results, use
+	// the WithAllMatches flag. To see all results with the highest score use
+	// the WithAllBestResults flag.
 	DataSources []int `json:"dataSources"`
 
-	// WithAllMatches provides all results, instead of only the BestResult.
-	// The results are sorted by score, not by data-source. The top result is
-	// the the best result.
+	// WithAllMatches when true, returns all results instead of only the best result.
+	// The results are sorted by score, not by data source. The top result is
+	// the best result.
 	WithAllMatches bool `json:"withAllMatches"`
 
 	// WithAllBestResults flag; when true, returns all results with the highest
 	// score instead of just one best result. This provides multiple equally
-	// good matches when they exist.
+	// good matches when they exist. This flag is ignored when WithAllMatches is
+	// true.
 	WithAllBestResults bool `json:"withAllBestResults"`
 
-	// Vernaculars field provides the list of languages to limit vernacular
-	// names result to only these languages. If 'all' is provided instead, all
-	// languages are included. Empty list means that no vernacular names will be
+	// Vernaculars contains the list of languages to limit vernacular
+	// name results to only these languages. If 'all' is provided as a 'language',
+	//  all languages are included. An empty list means no vernacular names will be
 	// returned.
 	Vernaculars []string `json:"vernaculars"`
 
-	// WithCapitalization flag; when true, the first rune of low-case
-	// input name-strings will be capitalized if appropriate.
+	// WithCapitalization when true, capitalizes the first letter of lowercase
+	// input name-strings when appropriate.
 	WithCapitalization bool `json:"withCapitalization"`
 
-	// WithSpeciesGroup flag; when true, species names also get matched by
-	// their species group. It means that the request will take in account
-	// botanical autonyms and zoological coordinated names.
+	// WithSpeciesGroup when true, enables species names to be matched by
+	// their species group. This includes botanical autonyms and zoological
+	// coordinated names in the matching process.
 	WithSpeciesGroup bool `json:"withSpeciesGroup"`
 
-	// WithRelaxedFuzzyMatch flag; when true, the fuzzy matching rules are
-	// relaxed. Normally it is switched off to decrease the number of false
-	// positives and make verification faster.
+	// WithRelaxedFuzzyMatch when true, relaxes the fuzzy matching rules.
+	// Normally disabled to reduce false positives and improve verification speed.
 	WithRelaxedFuzzyMatch bool `json:"withRelaxedFuzzyMatch"`
 
-	// WithUninomialFuzzyMatch flag; when true, uninomial names are not
-	// restricted from fuzzy matching. Normally it creates too many false
-	// positives and is switched off.
+	// WithUninomialFuzzyMatch when true, allows uninomial names to undergo
+	// fuzzy matching. Normally disabled as it generates too many false positives.
 	WithUninomialFuzzyMatch bool `json:"withUninomialFuzzyMatch"`
 
-	// WithStats flag; when true, results will return the most prevalent
-	// kingdom for the text, as well as the taxon which contains a given
-	// percentage of all names in the text (MainTaxon).
+	// WithStats when true, includes statistical information in the results,
+	// such as the most prevalent kingdom and the taxon containing a specified
+	// percentage of all names (MainTaxon).
 	//
-	// For example MainTaxon with the MainTaxonThreshold of 0.5 would correspond
-	// to a taxon that contains at least half of all names. We use the
-	// managerial classification of Catalogue of Life for the MainTaxon
-	// calculation.
+	// For example, a MainTaxon with MainTaxonThreshold of 0.5 corresponds
+	// to a taxon containing at least half of all names. The managerial
+	// classification of Catalogue of Life is used for MainTaxon calculations.
 	WithStats bool `json:"withStats"`
 
-	// MainTaxonThreshold sets the minimal percentage of names in a taxon
-	// to be counted as a MainTaxon of a text. This field is ignored if
+	// MainTaxonThreshold sets the minimum percentage of names a taxon must
+	// contain to qualify as the MainTaxon. This field is ignored if
 	// WithStats is false.
 	//
-	// MainTaxon is a taxon that contains at least MainTaxonThreshold percentage
-	// of all names (genus and below) in the text. We use the managerial
-	// classification of Catalogue of Life for the MainTaxon calculation.
+	// MainTaxon is a taxon containing at least MainTaxonThreshold percentage
+	// of all names (genus level and below). The managerial classification
+	// of Catalogue of Life is used for MainTaxon calculations.
 	MainTaxonThreshold float32 `json:"mainTaxonThreshold"`
 }
 
-// Output is a result returned by Verify method.
+// Output contains the result returned by the Verify method.
 type Output struct {
-	// Meta is the metadata of the request results.
+	// Meta contains the metadata of the request results.
 	Meta `json:"metadata"`
-	// Names are results of name-verification.
+	// Names contains the results of name verification.
 	Names []Name `json:"names"`
 }
 
-// Meta is metadata of the request. It provides information about parameters
-// used for the request, and, optionally give information about the kingdom
-// that contains most of the names from the request, as well as the lowest
-// taxon that contains majority of the names.
+// Meta contains metadata about the request, including information about parameters
+// used for the request. Optionally provides information about the kingdom
+// containing most names from the request, as well as the lowest
+// taxon containing the majority of the names.
 type Meta struct {
-	// NamesNumber is the number of name-strings in the request.
+	// NamesNumber contains the total number of name-strings in the request.
 	NamesNumber int `json:"namesNumber"`
 
-	// Vernaculars tells which languages were requested for finding vernacular
-	// names.
+	// Vernaculars contains the languages that were requested for finding
+	// vernacular names.
 	Vernaculars []string `json:"vernaculars,omitempty"`
 
-	// WithAllSources indicates if `Results` will include all matched
-	// sources.
+	// WithAllSources indicates whether results will include all matched
+	// data sources.
 	WithAllSources bool `json:"withAllSources,omitempty"`
 
-	// WithAllMatches indicates if response provides more then one result
-	// per source, if such results were found.
+	// WithAllMatches indicates whether the response provides more than one result
+	// per source when such results are found.
 	WithAllMatches bool `json:"withAllMatches,omitempty"`
 
-	// WithAllBestResults indicates that response will return a slice of
-	// all results with the highest score. This flag should not be shown
-	// when WithAllMatches is true, because all best results will be
-	// alreadyincluded into all matches.
+	// WithAllBestResults indicates that the response will return all
+	// results with the highest score. This flag should not be shown
+	// when WithAllMatches is true, as all best results will be
+	// already included in all matches.
 	WithAllBestResults bool `json:"withAllBestResults,omitempty"`
 
-	// WithStats indicates that the kingdom and a taxon that contain
-	// majority of names (MainTaxon) will be calculated.
+	// WithStats indicates that statistical information including the kingdom
+	// and taxon containing the majority of names (MainTaxon) will be calculated.
 	WithStats bool `json:"withStats,omitempty"`
 
 	// WithCapitalization is true if there was a request to capitalize input
+	// name-strings.
 	WithCapitalization bool `json:"withCapitalization,omitempty"`
 
-	// WithSpeciesGroup is true if Input included `WithSpeciesGroup` option.
+	// WithSpeciesGroup is true if the input included the WithSpeciesGroup option.
 	WithSpeciesGroup bool `json:"withSpeciesGroup,omitempty"`
 
-	// WithRelaxedFuzzyMatch is true if Input included `WithRelaxedFuzzyMatch`
-	// option. It means that the fuzzy matching rules are relaxed.
-	// Normally it is switched off to decrease the number of false positives.
+	// WithRelaxedFuzzyMatch is true if the input included the WithRelaxedFuzzyMatch
+	// option, meaning the fuzzy matching rules are relaxed.
+	// Normally disabled to reduce false positives.
 	WithRelaxedFuzzyMatch bool `json:"withRelaxedFuzzyMatch,omitempty"`
 
-	// WithUninomialFuzzyMatch is true when it when uninomial names go
-	// through fuzzy matching. Normally it is switched off to decrease the
-	// number of false positives.
+	// WithUninomialFuzzyMatch is true when uninomial names undergo
+	// fuzzy matching. Normally disabled to reduce false positives.
 	WithUninomialFuzzyMatch bool `json:"withUninomialFuzzyMatch,omitempty"`
 
-	// DataSources provides IDs of data-sources from the request.
+	// DataSources contains the IDs of data sources from the request.
 	DataSources []int `json:"dataSources,omitempty"`
 
-	// MainTaxonThreshold provides a minimal percentage names that a taxon should
-	// have to be qualified as a MainTaxon.
+	// MainTaxonThreshold contains the minimum percentage of names a taxon
+	// must have to qualify as the MainTaxon.
 	MainTaxonThreshold float32 `json:"mainTaxonThreshold,omitempty"`
 
-	// StatsNamesNum is the number of names qualified for MainTaxon/Kingdoms
-	// calculation.
+	// StatsNamesNum contains the number of names that qualified for MainTaxon
+	// and Kingdom calculations.
 	StatsNamesNum int `json:"statsNamesNum,omitempty"`
 
-	// MainTaxon provides the lowest taxon that contains most of the names from
+	// MainTaxon contains the lowest taxon that includes most of the names from
 	// the request.
 	//
-	// Non-matched names, names that are not in the Catalogue of Life, names
-	// higher than genus are not part of the calculation.
+	// Non-matched names, names not in the Catalogue of Life, and names
+	// higher than genus level are not included in the calculation.
 	MainTaxon string `json:"mainTaxon,omitempty"`
 
-	// MainTaxonPercentage indicates the percentage of names that are placed
-	// in the MainTaxon. This number should be higher than
+	// MainTaxonPercentage contains the percentage of names placed
+	// in the MainTaxon. This value should be higher than
 	// MainTaxonThreshold unless MainTaxon is empty.
 	MainTaxonPercentage float32 `json:"mainTaxonPercentage,omitempty"`
 
-	// Kingdom provides what kingdom includes the majority of names from the
-	// request accorging to the managerial classification of Catalogue of Life.
+	// Kingdom contains the kingdom that includes the majority of names from the
+	// request according to the managerial classification of Catalogue of Life.
 	//
-	// Non-matched names, or names that are not in Catalogue of Life are
-	// not part of the calculation.
+	// Non-matched names and names not in the Catalogue of Life are
+	// not included in the calculation.
 	Kingdom string `json:"kingdom,omitempty"`
 
-	// KingdomPercentage provides the percentage of names in the most
+	// KingdomPercentage contains the percentage of names in the most
 	// prevalent kingdom.
 	//
-	// Non-matched names, or names that are not in Catalogue of Life are
-	// not part of the calculation.
+	// Non-matched names and names not in the Catalogue of Life are
+	// not included in the calculation.
 	KingdomPercentage float32 `json:"kingdomPercentage,omitempty"`
 
-	// Kingdoms provides all kingdoms with matched names and names distribution
-	// between the kingdoms.
+	// Kingdoms contains all kingdoms with matched names and their
+	// distribution statistics.
 	Kingdoms []Kingdom `json:"kingdoms,omitempty"`
 }
 
-// Kingdom provides statistics of matched names found in a particular kingdom.
+// Kingdom contains statistics for matched names found in a particular kingdom.
 type Kingdom struct {
-	// KingdomName is the name of a kingdom.
+	// KingdomName contains the name of the kingdom.
 	KingdomName string `json:"kingdomName"`
 
-	// NamesNumber is the number of names found in a kingdom.
+	// NamesNumber contains the number of names found in this kingdom.
 	NamesNumber int `json:"namesNumber"`
 
-	// Percentage is a percentage of names found in a kingdom.
+	// Percentage contains the percentage of names found in this kingdom.
 	Percentage float32 `json:"percentage"`
 }
 
-// NameStringInput is used to get information about a particular name-string.
+// NameStringInput contains parameters for retrieving information about a particular name-string.
 type NameStringInput struct {
-	// ID is the UUID v5 generated from a name-string
+	// ID contains the UUID v5 generated from a name-string.
 	ID string
 
-	// DataSources is a slice of DataSourceIDs which should be used in the
-	// output. If the slice is empty, all DataSources are used.
+	// DataSources contains data source IDs to be used in the
+	// output. If empty, all data sources are used.
 	DataSources []int
 
-	// WithAllMatches controls if only the BestMatch, or all possible matches
+	// WithAllMatches controls whether only the best match or all possible matches
 	// are returned.
 	WithAllMatches bool
 }
 
-// NameStringOutput contains data corresponding to the provided name-string ID.
+// NameStringOutput contains the data corresponding to the provided name-string ID.
 type NameStringOutput struct {
-	// NameStringMeta contains metadata from the input.
+	// NameStringMeta contains the metadata from the input.
 	NameStringMeta `json:"meta"`
 
-	// Name is the found name data.
+	// Name contains the found name data.
 	*Name `json:"name"`
 }
 
-// NameStringMeta contains metadata from the provided input.
+// NameStringMeta contains the metadata from the provided input.
 type NameStringMeta struct {
-	// ID is the UUID v5 generated for a particular name-string.
+	// ID contains the UUID v5 generated for a particular name-string.
 	ID string `json:"id"`
 
-	// DataSources is a slice of DataSource IDs. If it is not empty,
-	// the output results will be constrained to these IDs.
+	// DataSources contains data source IDs. If not empty,
+	// the output results will be limited to these IDs.
 	DataSources []int `json:"dataSources,omitempty"`
 
-	// WithAllMatches indicates if all matches should be returned, or
-	// only the best matches.
+	// WithAllMatches indicates whether all matches should be returned
+	// or only the best matches.
 	WithAllMatches bool `json:"withAllMatches"`
 }
